772092fd94b7f81ba72e43d6fb880c1b1653854c
HamoGhazoyan1 <hm.ghazoyan@gmail.com>
2020-03-03 14:08:02 +0400

    First commit

--- Homework5/src/Problem6.java
+++ Homework5/src/Problem6.java
@@ -1,12 +1,12 @@
 public class Problem6 {
     public static void main(String[] args) {
-        int[] arr = new int[]{55, 44, 88, 99, 66, 32, 15, 1};
+        int[] arr = new int[]{0,55, 44, 88, 99, 66, 32, 15, 1};
         int max = arr[0];
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > arr[i - 1]) {
+            if (arr[i] > max) {
                 max = arr[i];
-            } else if (arr[i] < arr[i - 1]) {
+            } else if (arr[i] < min) {
                 min = arr[i];
             }
         }
